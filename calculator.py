f99e86fdc6cacb3f342c8209990c8690c9f340d9
Vibhucoding <122472911+Vibhucoding@users.noreply.github.com>
2023-01-11 22:33:05 +0530

    Add files via upload
    It is a python based calculator

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("---->>>Welcome to the calculator application<<<----")
+print("\n")
+print("---->choose one option from below<-----")
+print("option1-for-addition")
+print("option2-for-subtraction")
+print("option3-for-multiplication")
+print("option4-for-division")
+a= int(input("enter the no. of the option you wanna operate with "))
+print("you choose option no.-->",a)
+if a>=5:
+    print(" An invalid option was choosen by you ")
+else:
+    
+    b= int(input("enter the first no. you want to operate with "))
+    c= int(input("enter the second no. you want to operate with "))
+    if a == 1:
+        print(+cb)
+    elif a== 2:
+        print(b-c)
+    elif a==3:
+        print(c*b)
+    elif a==4:
+        print(b/c)
+
+    
